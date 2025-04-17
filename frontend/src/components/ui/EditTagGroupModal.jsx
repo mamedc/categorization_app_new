95589891b5bb77aa467facec196d6f8c37b84bba
Mamed Caki <mamedcaki@gmail.com>
2025-04-17 11:55:42 -0300

    edit tagGroup logica selecao tags

--- frontend/src/components/ui/EditTagGroupModal.jsx
+++ frontend/src/components/ui/EditTagGroupModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BASE_URL } from "../../App"
-import { Button, Dialog, Field, Input, Portal, Flex, Textarea, CloseButton, Spinner, VStack, Text } from "@chakra-ui/react"
+import { Button, Dialog, Field, ColorSwatch , Portal, Flex, Checkbox, CloseButton, Spinner, VStack, HStack, Text } from "@chakra-ui/react"
 import { Toaster, toaster } from "@/components/ui/toaster"
 
 
@@ -12,6 +12,14 @@ export default function EditTagGroupModal ({
     const [isLoading, setIsLoading] = useState(false);
     const [loadError, setLoadError] = useState('');
     const [formData, setFormData] = useState({ name: '', tags: [] });
+    const [isSelectedTag, setIsSelectedTag] = useState(false);
+    const [selectedTagId, setSelectedTagId] = useState(null);
+    
+    const handleSelectTag = (tagId) => {
+        setSelectedTagId((prevSelectedId) =>
+            prevSelectedId === tagId ? null : tagId
+        );
+    };
 
     const handleInitialTagGroupState = async () => {
         setIsLoading(true); // Start loading
@@ -33,13 +41,13 @@ export default function EditTagGroupModal ({
                 tags: [...data.tags].sort((a, b) => a.name.localeCompare(b.name)),
             });
             // setTagGroups((prevGroup) => prevGroup.filter((u) => u.id !== selectedTagGroupId));
-            toaster.create({
-                title: "Success!",
-                description: "Tag Group loaded.",
-                type: "success",
-                duration: 2000,
-                placement: "top-center",
-            })
+            // toaster.create({
+            //     title: "Success!",
+            //     description: "Tag Group loaded.",
+            //     type: "success",
+            //     duration: 2000,
+            //     placement: "top-center",
+            // })
         } catch (error) {
             console.error(error);
             toaster.create({
@@ -56,13 +64,19 @@ export default function EditTagGroupModal ({
     
     const handleOpen = () => {
         setOpen(true);
+        setIsSelectedTag(false);
+        setSelectedTagId(null);
         handleInitialTagGroupState();
     };
     const handleClose = () => {
         setOpen(false);
+        setIsSelectedTag(false);
+        setSelectedTagId(null);
         setFormData({ name: '', tags: [] });
         
     };
+
+
 
     // const handleChange = (e) => {
     //     const { name, value } = e.target;
@@ -129,7 +143,7 @@ export default function EditTagGroupModal ({
                     <Dialog.Backdrop />
                     <Dialog.Positioner>
                         <Dialog.Content>
-                            <Dialog.Header><Dialog.Title>{formData.name}</Dialog.Title></Dialog.Header>
+                            <Dialog.Header><Dialog.Title>{formData.name} group</Dialog.Title></Dialog.Header>
                             <Dialog.Body pb="4">
                                 {isLoading && (
                                     <Flex justify="center" mt={8}>
@@ -148,21 +162,50 @@ export default function EditTagGroupModal ({
                                 {!isLoading && formData.tags.length > 0 && (
                                     <VStack spacing={6} align="stretch" > {/* Add spacing between date groups */}
                                         {formData.tags.map((tag) => (
-                                            <Text 
+                                            <Flex
                                                 key={tag.id}
-                                                //tag={tag}
-                                                //isSelectedTag={isSelectedTag}
-                                                //onSelectTag={onSelectTag}
+                                                direction={'row'}
+                                                align={{ base: 'start', md: 'center' }}
+                                                gap={4}
+                                                wrap="wrap"
                                             >
-                                                {tag.name}
-                                            </Text>
+                                                {/*Checkbox*/}
+                                                {/* Control the checked state and handle changes */}
+                                                <Checkbox.Root
+                                                    variant="outline"
+                                                    size="sm"
+                                                    colorPalette="cyan"
+                                                    mt={{ base: 1, md: 0 }}
+                                                    checked={tag.id === selectedTagId}
+                                                    onCheckedChange={() => handleSelectTag(tag.id)}
+                                                >
+                                                    <Checkbox.HiddenInput />
+                                                    <Checkbox.Control />
+                                                </Checkbox.Root>
+
+                                                {/* Left: Details */}
+                                                <VStack align="start" spacing={1} flex="1">
+                                                    <HStack spacing={3} wrap="wrap">
+                                                        <Text fontSize="sm" color="gray.500">
+                                                            {tag.name}
+                                                        </Text>
+                                                    </HStack>
+                                                </VStack>
+
+                                                {/* Right: Value + Flags */}
+                                                <VStack align="end" spacing={1}>
+                                                    <ColorSwatch value={tag.color} size="xs" borderRadius="xl" />
+                                                </VStack>
+
+                                            </Flex>
+
                                         ))}
                                     </VStack>
                                 )}  
                             </Dialog.Body>
                             
                             <Dialog.Footer>                          
-                                {/* <Dialog.ActionTrigger asChild>
+                                <Dialog.ActionTrigger asChild>
                                     <Button 
                                         variant="surface" 
                                         onClick={handleClose} 
@@ -172,7 +215,7 @@ export default function EditTagGroupModal ({
                                     </Button>
                                 </Dialog.ActionTrigger>
                                 
-                                <Button
+                                {/* <Button
                                     onClick={handleEditUser} 
                                     disabled={isLoading}
                                 >
