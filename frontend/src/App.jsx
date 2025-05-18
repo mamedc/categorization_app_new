94833ad4e509edd6e2c1f15a9bfd7c6321ba2425
mamedc <43475964+mamedc@users.noreply.github.com>
2025-05-18 06:35:23 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import { AuthProvider, useAuth } from './context/AuthContext';
 import { Login } from './components/auth/Login';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
 
-export const BASE_URL = "http://127.0.0.1:5000/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
 
 // Layout for protected routes that includes Navbar
 const ProtectedLayout = () => {
